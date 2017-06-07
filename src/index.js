cf3293a8ce9b22631536a70cd04d40c9261718e0
silky098 <shaila@ccwd.com.au>
2017-06-07 22:48:41 +1000

    constructor in place

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
-const API_KEY = 'AIzaSyD5LQbyNgAGKUc7-cq9-5-kqkK4_UVQGGg';
+// const API_KEY = 'AIzaSyD5LQbyNgAGKUc7-cq9-5-kqkK4_UVQGGg';
 
 // component needs to be rendered to be placed into the Dom
 
 const App = () => {
-  return <div>Hi!</div>
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
