143c5fab9c419c6dd42df4f03e38a7b744953864
silky098 <shaila@ccwd.com.au>
2017-06-07 14:31:11 +1000

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyD5LQbyNgAGKUc7-cq9-5-kqkK4_UVQGGg';
+
+// component needs to be rendered to be placed into the Dom
+
+const App = () => {
+  return <div>Hi!</div>
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
